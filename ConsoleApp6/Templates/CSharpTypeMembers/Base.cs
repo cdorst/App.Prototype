5737523f8e05d2552c5c296a630a8facf595e475
Christopher Dorst <chris@dorst.email>
2018-03-17 14:13:26 -0500

    WIP: Dependency update; add member types; add Azure Table class libraries

---
+++ ConsoleApp6/Templates/CSharpTypeMembers/Base.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace ConsoleApp6.Templates.CSharpTypeMembers
+{
+    public class Base
+    {
+        public string Name { get; set; }
+        public List<string> TypeArguments { get; set; }
+    }
+}
--- ConsoleApp6/Templates/CSharpTypeMembers/Method.cs
+++ ConsoleApp6/Templates/CSharpTypeMembers/Method.cs
@@ -4,10 +4,23 @@ namespace ConsoleApp6.Templates.CSharpTypeMembers
 {
     public class Method
     {
+        public string ArrowClauseExpression { get; set; }
         public List<Attribute> Attributes { get; set; }
+        public List<string> Block { get; set; }
         public string Comment { get; set; }
+        public string Modifiers { get; set; }
         public string Name { get; set; }
         public List<Parameter> Parameters { get; set; }
+        public string Type { get; set; }
+        public List<string> TypeParameters { get; set; }
+    }
+
+    public class Property
+    {
+        public List<Attribute> Attributes { get; set; }
+        public string Comment { get; set; }
+        public string Modifiers { get; set; }
+        public string Name { get; set; }
         public string Type { get; set; }
     }
 }
